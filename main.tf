627c0c6ca79693265d9770f7d653f1700af9d7d5
Ubuntu <ubuntu@ip-172-31-27-202.us-east-2.compute.internal>
2022-09-28 15:51:28 +0000

    2nd initial commit

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "web" {
   tags = {
     Name = "docker-instance-${count.index + 1}"
   }
-
+}
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
